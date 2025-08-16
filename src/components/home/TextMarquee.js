4eddb682445a73d58056b16ff7c2caabeadc709c
Abdul Rafay Farooqui <farooquiabdul85@gmail.com>
2025-08-16 21:03:55 +0500

    added marquee with diffrent way

--- src/components/home/TextMarquee.js
+++ src/components/home/TextMarquee.js
@@ -1,34 +1,14 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 
 const TextMarquee = () => {
-  const [position, setPosition] = useState(window.innerWidth);
-  const requestRef = useRef(null);
-
-  useEffect(() => {
-    const speed = 2; // pixels per frame
-
-    const animate = () => {
-      setPosition((prev) => {
-        if (prev < -1000) return window.innerWidth; // reset when off screen
-        return prev - speed;
-      });
-      requestRef.current = requestAnimationFrame(animate);
-    };
-
-    requestRef.current = requestAnimationFrame(animate);
-
-    return () => cancelAnimationFrame(requestRef.current);
-  }, []);
-
   return (
     <section className="bg-blue-900 overflow-hidden relative h-12">
-      <p
-        className="absolute text-white rubik text-lg whitespace-nowrap top-1/2 -translate-y-1/2"
-        style={{ left: position }}
-      >
-        Join our webinar: Achieving NEPRA Cybersecurity Compliance with Kaspersky
-      </p>
+      <div className="whitespace-nowrap absolute top-1/2 -translate-y-1/2 animate-marquee">
+        <p className="text-white rubik text-lg inline-block px-8">
+          Join our webinar: Achieving NEPRA Cybersecurity Compliance with Kaspersky
+        </p>
+      </div>
     </section>
   );
 };
