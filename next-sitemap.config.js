910af9846a5f45b4efeb85fd35db47fb90225bbf
Abdul Rafay Farooqui <farooquiabdul85@gmail.com>
2025-08-24 15:43:27 +0500

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://nepra.netlify.app", // your deployed site URL
+  generateRobotsTxt: true, // also generates robots.txt
+  sitemapSize: 5000, // optional, splits large sitemaps
+};
